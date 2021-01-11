83f2d1ee5a4b463927e57194f17e7e05fd02ff86
tp0ns <tpons@student.42.fr>
2021-01-11 13:37:42 +0100

    Norm main

--- main.c
+++ main.c
@@ -6,25 +6,25 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 12:04:28 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/11 13:19:03 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/11 13:37:22 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main()
+int	main(void)
 {
-    while (1)
-    {
-		char *line;
+	char *line;
 
+	while (1)
+	{
 		line = malloc(sizeof(char));
 		if (!line)
 			return (1);
-        ft_putstr("piti_prompt>");
+		ft_putstr("minishell>");
 		get_next_line(0, &line);
 		ft_putstr(line);
 		ft_putstr("\n");
-    }
-    return (0);
+	}
+	return (0);
 }
