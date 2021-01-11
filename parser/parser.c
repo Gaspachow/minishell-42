00ad5128549ea6402cdcdc2cf16b4d095b764137
gsmets <gsmets@student.42.fr>
2021-01-11 18:34:39 +0100

    parser folder

--- parser/parser.c
+++ parser/parser.c
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/11 12:04:28 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/11 17:45:39 by gsmets           ###   ########.fr       */
+/*   Created: 2021/01/11 17:45:05 by gsmets            #+#    #+#             */
+/*   Updated: 2021/01/11 17:45:32 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../minishell.h"
 
-int	main(void)
+int	parser(char **inputs)
 {
-	char	*user_input;
-	char	**inputs;
-	int		i;
-
-	user_input = NULL;
-	inputs= NULL;
-	while (1)
-	{
-		i = 0;
-		ft_putstr("minishell> ");
-		get_next_line(0, &user_input);
-		inputs = ft_split(user_input, ' ');
-		parser(inputs);
-
-	}
+	if (!*inputs)
+		return (0);
+	if (!ft_strcmp(inputs[0], "echo"))
+		handle_echo(inputs);
+	else
+		write(1, "try again\n", 10);
 	return (0);
 }
