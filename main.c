2da30586178eedfa55b98a16953359cc015cf26f
tp0ns <tpons@student.42.fr>
2021-01-11 13:19:38 +0100

    Prompt first step

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 12:04:28 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/11 12:33:25 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/11 13:19:03 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int main()
 {
-    ft_putstr("coucou");
+    while (1)
+    {
+		char *line;
+
+		line = malloc(sizeof(char));
+		if (!line)
+			return (1);
+        ft_putstr("piti_prompt>");
+		get_next_line(0, &line);
+		ft_putstr(line);
+		ft_putstr("\n");
+    }
     return (0);
 }
