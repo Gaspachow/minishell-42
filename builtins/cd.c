0ca9c4ff98e8759acd6dd424664acf0d34064002
tp0ns <tpons@student.42.fr>
2021-01-13 12:24:06 +0100

    Norm cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,15 +6,21 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 13:15:19 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/12 14:04:03 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/13 12:23:49 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/*
+**	In handle_cd :
+**		chdir returns -1 if it fails to change directory
+**		return(0) === error
+*/
+
 int		handle_cd(char **args)
 {
-	if(chdir(args[1]) == -1) //return -1 if it fails to change directory
-		return(0); //error
+	if (chdir(args[1]) == -1)
+		return (0);
 	return (1);
 }
