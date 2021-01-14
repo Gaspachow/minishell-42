c15d5717dce2a8ad56dee1cdc8babbba2913c5ec
gsmets <gsmets@student.42.fr>
2021-01-14 14:51:45 +0100

    fix pipe fd

--- builtins/pipe.c
+++ builtins/pipe.c
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 15:50:16 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/13 19:59:15 by gsmets           ###   ########.fr       */
+/*   Updated: 2021/01/14 14:41:29 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ int		handle_pipe(char *input1, char *input2, char **env)
 	int		fds[2];
 	int		status;
 
+	oldfd = dup(1);
 	if (pipe(fds) < 0)
 		exit(EXIT_FAILURE);// return (0);
 	pid = fork();
 	if (pid == 0)
 	{
-		oldfd = dup(1);
 		dup2(fds[1], 1);
 		parser_delegator(input1, env, 1);
 	}
@@ -36,11 +36,13 @@ int		handle_pipe(char *input1, char *input2, char **env)
 		exit(EXIT_FAILURE);//return (0);
 	else
 	{
+		dup2(oldfd, 1);
 		if (waitpid(pid, &status, 0) != pid)
 			exit(EXIT_FAILURE);//return (0);
 		oldfd = dup(0);
 		dup2(fds[0], 0);
 		parser_start(input2, env);
+		dup2(oldfd, 0);
 	}
 	return (1);
 }
