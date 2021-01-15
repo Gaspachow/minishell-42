4784886f6eca61875b02d876f9f00662a2136a03
tp0ns <tpons@student.42.fr>
2021-01-15 17:40:57 +0100

    Export improvement (now replace existing var)

--- builtins/export.c
+++ builtins/export.c
@@ -6,11 +6,36 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 15:35:31 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/15 16:51:49 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/15 17:35:47 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int		var_exists(char *name, t_data *data)
+{
+	int		y;
+	int		x;
+
+	x = 0;
+	while (data->env[x])
+	{
+		y = 0;
+		while (data->env[x][y] && data->env[x][y] == name[y]
+		&& data->env[x][y] != '=' && name[y] != '=')
+			y++;
+		if (data->env[x][y] == '=' && name[y] == '=')
+			return (x);
+		x++;
+	}
+	return (0);
+}
+
+void	replace_var(char *new_var, t_data *data, int index)
+{
+	free(data->env[index]);
+	data->env[index] = ft_strdup(new_var);
+}
 
 char	**export_env(char **old_env, char *export)
 {
@@ -36,15 +61,22 @@ char	**export_env(char **old_env, char *export)
 int		handle_export(char **inputs, t_data *data)
 {
 	int	i;
+	int	index;
 
 	i = 1;
 	if (inputs[i])
 	{
 		while (inputs[i])
 		{
-			data->env = export_env(data->env, inputs[i]);
-			if (!data->env)
-				exit(EXIT_FAILURE);
+			index = var_exists(inputs[i], data);
+			if (index > 0)
+				replace_var(inputs[i], data, index);
+			else
+			{
+				data->env = export_env(data->env, inputs[i]);
+				if (!data->env)
+					exit(EXIT_FAILURE);
+			}
 			i++;
 		}
 	}
