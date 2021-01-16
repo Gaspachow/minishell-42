08f3ce10fdf762ada03c3588dae55aafd276bb90
gsmets <gsmets@student.42.fr>
2021-01-16 13:36:25 +0100

    parser handles "$?' variable

--- parser/parser_variable.c
+++ parser/parser_variable.c
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 12:35:13 by gsmets            #+#    #+#             */
-/*   Updated: 2021/01/15 23:26:41 by gsmets           ###   ########.fr       */
+/*   Updated: 2021/01/16 13:00:01 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ static int		get_name_len(char *str)
 
 	len = 0;
 	while (ft_isalnum(str[len]) || str[len] == '_')
-	{
 		len++;
-	}
+	if (!len && str[len] == '?')
+		return (1);
 	return (len);
 }
 
 static char		*get_value(char *name, t_data *data)
 {
 	char	**env;
-	int 	i;
+	int		i;
 	int		j;
 	int		k;
 
@@ -43,7 +43,7 @@ static char		*get_value(char *name, t_data *data)
 			j++;
 		}
 		if (env[i][j] == '=' && !name[k])
-			return(ft_strdup(&env[i][j + 1]));
+			return (ft_strdup(&env[i][j + 1]));
 		i++;
 	}
 	return (NULL);
@@ -58,8 +58,10 @@ void			parser_variable(char **input_address, int *i, t_data *data)
 	char	*tmp;
 
 	len = get_name_len(&(input_address[0][*i + 1]));
-	var_name = ft_substr(*input_address,*i + 1, len);
-	if (len)
+	var_name = ft_substr(*input_address, *i + 1, len);
+	if (len == 1 && input_address[0][*i + 1])
+		var_value = ft_strdup(ft_itoa(data->status));
+	else if (len)
 		var_value = get_value(var_name, data);
 	else
 		var_value = ft_strdup("$");
