251164af1c6744083573f0cc0022427e0627e002
tp0ns <tpons@student.42.fr>
2021-01-18 12:55:24 +0100

    var_exists is now var_index

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 13:15:19 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/16 16:58:33 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/18 11:21:01 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,18 @@ int		handle_cd(char **args, t_data *data)
 	char	*pwd;
 	char	*oldpwd;
 
-	if (chdir(args[1]) == -1 && var_exists("PWD=", data) &&
-	var_exists("OLDPWD=", data))
+	if (chdir(args[1]) == -1 && var_index("PWD=", data) &&
+	var_index("OLDPWD=", data))
 		return (0);
 	else
 	{
-		oldpwd = ft_strjoin("OLD", data->env[var_exists("PWD=", data)]);
-		replace_var(oldpwd, data, var_exists("OLDPWD=", data));
+		oldpwd = ft_strjoin("OLD", data->env[var_index("PWD=", data)]);
+		replace_var(oldpwd, data, var_index("OLDPWD=", data));
 		free(oldpwd);
 		buf = getcwd(NULL, 0);
 		pwd = ft_strjoin("PWD=", buf);
 		free(buf);
-		replace_var(pwd, data, var_exists(pwd, data));
+		replace_var(pwd, data, var_index(pwd, data));
 	}
 	return (1);
 }
