5a04df1233ba3b92658b578c4f77c41ac470a174
tp0ns <tpons@student.42.fr>
2021-01-21 18:59:10 +0100

    cd behaves properly

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 13:15:19 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/18 11:21:01 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/21 17:52:07 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,85 @@
 **		return(0) === error
 */
 
-int		handle_cd(char **args, t_data *data)
+int		cd_minus(t_data *data)
 {
 	char	*buf;
 	char	*pwd;
 	char	*oldpwd;
 
-	if (chdir(args[1]) == -1 && var_index("PWD=", data) &&
-	var_index("OLDPWD=", data))
-		return (0);
+	if (var_index("PWD=", data) < 0 || var_index("OLDPWD=", data) < 0 ||
+	chdir((strchr(data->env[var_index("OLDPWD=", data)], '=') + 1)) == -1)
+		return (0);//error
+	oldpwd = ft_strjoin("OLD", data->env[var_index("PWD=", data)]);
+	replace_var(oldpwd, data, var_index("OLDPWD=", data));
+	free(oldpwd);
+	buf = getcwd(NULL, 0);
+	pwd = ft_strjoin("PWD=", buf);
+	free(buf);
+	replace_var(pwd, data, var_index(pwd, data));
+	free(pwd);
+	return (1);
+}
+
+int		cd_alone(t_data *data)
+{
+	char	*buf;
+	char	*pwd;
+	char	*oldpwd;
+
+	if (var_index("HOME=", data) < 0 ||
+	chdir((strchr(data->env[var_index("HOME=", data)], '=') + 1)) == -1 ||
+	var_index("PWD=", data) < 0 || var_index("OLDPWD=", data) < 0)
+		return (0);//error
+	oldpwd = ft_strjoin("OLD", data->env[var_index("PWD=", data)]);
+	replace_var(oldpwd, data, var_index("OLDPWD=", data));
+	free(oldpwd);
+	buf = getcwd(NULL, 0);
+	pwd = ft_strjoin("PWD=", buf);
+	free(buf);
+	replace_var(pwd, data, var_index(pwd, data));
+	free(pwd);
+	return (1);
+}
+
+int		cd_path(char **args, t_data *data)
+{
+	char	*buf;
+	char	*pwd;
+	char	*oldpwd;
+
+	if (chdir(args[1]) == -1 || var_index("PWD=", data) < 0 ||
+	var_index("OLDPWD=", data) < 0)
+		return (0);//error
+	oldpwd = ft_strjoin("OLD", data->env[var_index("PWD=", data)]);
+	replace_var(oldpwd, data, var_index("OLDPWD=", data));
+	free(oldpwd);
+	buf = getcwd(NULL, 0);
+	pwd = ft_strjoin("PWD=", buf);
+	free(buf);
+	replace_var(pwd, data, var_index(pwd, data));
+	free(pwd);
+	return (1);
+}
+
+int		handle_cd(char **args, t_data *data)
+{
+	if (args[2])
+		return (0);//error
+	else if (!args[1])
+	{
+		if (!cd_alone(data))
+			return (0);
+	}
+	else if (ft_strcmp(args[1], "-") == 0)
+	{
+		if (!cd_minus(data))
+			return (0);
+	}
 	else
 	{
-		oldpwd = ft_strjoin("OLD", data->env[var_index("PWD=", data)]);
-		replace_var(oldpwd, data, var_index("OLDPWD=", data));
-		free(oldpwd);
-		buf = getcwd(NULL, 0);
-		pwd = ft_strjoin("PWD=", buf);
-		free(buf);
-		replace_var(pwd, data, var_index(pwd, data));
+		if (!cd_path(args, data))
+			return (0);
 	}
 	return (1);
 }
