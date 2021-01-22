73d0965cd57df1e2db84d9cbc0ee122ca7794acd
gsmets <gsmets@student.42.fr>
2021-01-22 17:38:33 +0100

    free_inputs function draft

--- parser/handle_basic.c
+++ parser/handle_basic.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_basic.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 19:01:43 by gsmets            #+#    #+#             */
-/*   Updated: 2021/01/22 16:36:56 by tpons            ###   ########.fr       */
+/*   Updated: 2021/01/22 17:36:08 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,21 @@ void		choose_action(char **inputs, t_data *data)
 		handle_exec(inputs, data);
 }
 
+void		free_inputs(char **inputs)
+{
+	int	i;
+
+	i = 0;
+	while (inputs[i])
+	{
+		write(1, inputs[i], ft_strlen(inputs[i]));
+		write(1, "\n", 1);
+		free(inputs[i]);
+		i++;
+	}
+	// free(inputs);
+}
+
 int			handle_basic(char *clean_input, t_data *data, int piped)
 {
 	char	**inputs;
@@ -45,6 +60,7 @@ int			handle_basic(char *clean_input, t_data *data, int piped)
 	inputs = input_split(clean_input);
 	free(clean_input);
 	choose_action(inputs, data);
+	free_inputs(inputs);
 	if (piped)
 		exit(0);
 	dup2(oldfd1, 1);
