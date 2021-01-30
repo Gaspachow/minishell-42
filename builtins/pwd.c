7aeb1cfd13cedad069030155b223d9f97743f648
tp0ns <tpons@student.42.fr>
2021-01-30 12:56:28 +0100

    Fix segfault in pwd when no PWD var

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 17:48:49 by tpons             #+#    #+#             */
-/*   Updated: 2021/01/25 17:34:05 by gsmets           ###   ########.fr       */
+/*   Updated: 2021/01/30 12:52:02 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 /*
 ** Function handle_pwd :
-** getcwd return an allocated string filled
-** with the path if param are a NULL string and size = 0
 */
 
 int	handle_pwd(t_data *data)
 {
+	int		index;
 	char	*buf;
 
-	buf = data->env[var_index("PWD", data)];
+	index = var_index("PWD", data);
+	if (index == -1)
+		return (0);
+	buf = data->env[index];
 	ft_putstr(strchr(buf, '=') + 1);
 	ft_putstr("\n");
 	return (0);
 }
-// need to handle arguments
